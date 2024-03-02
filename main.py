bd720a822c25f46e97f4bc0bf9e29cdcb1bae007
Faz Kay <fkydln@gmail.com>
2024-03-02 12:05:28 -0800

    Snake body can move up, down, right and left.
    Snake body cannot move opposite direction as it's not allowed in the official game.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # This is a simple Python based snake game.
 from turtle import Turtle, Screen
+from snake import Snake
 import time
-faz = Turtle(shape='square')
+
 screen = Screen()
 
 screen.setup(width=600, height=600)
@@ -9,27 +10,20 @@ screen.bgcolor('black')
 screen.title('My Snake Game')
 screen.tracer(0)
 
-faz.color('white')
-faz.penup()
-segments = []
-segments.append(faz)
-x_coordinate = 0
+snake = Snake()
 
-for turtle in range(3):
-    x_coordinate -= 20
-    new_turtle = Turtle(shape='square')
-    new_turtle.penup()
-    new_turtle.color('white')
-    new_turtle.goto(x_coordinate, 0)
-    segments.append(new_turtle)
+screen.listen()
 
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for seg in segments:
-        seg.forward(20)
+    snake.move()
 
 
 screen.exitonclick()

d02c6e10a7e5808fe0723adbdfd20244e8e8c072
Faz Kay <fkydln@gmail.com>
2024-03-04 21:59:50 -0800

    Game Over is added.

--- main.py
+++ main.py
@@ -35,5 +35,10 @@ while game_is_on:
         score += 1
         scoreboard.update_scoreboard(score)
 
+#     Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor()< -280:
+        game_is_on = False
+        scoreboard.game_over()
+
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
-
+ALIGNMENT = 'left'
+FONT = ("Courier", 32, "normal")
 class Scoreboard(Turtle):
 
     def __init__(self, score):
@@ -9,6 +10,10 @@ class Scoreboard(Turtle):
         self.penup()
         self.hideturtle()
         self.update_scoreboard(score)
+
+    def game_over(self):
+        self.goto(-80,0)
+        self.write("Game Over", align=ALIGNMENT, font=FONT)
 
     def update_scoreboard(self, score):
         self.clear()

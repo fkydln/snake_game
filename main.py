c8f159cbf5b19feff69f6c8b2d1c9f8de391155f
Faz Kay <fkydln@gmail.com>
2024-03-04 23:16:16 -0800

    MVP is done.

--- main.py
+++ main.py
@@ -29,16 +29,27 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-
+    #       Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
         score += 1
+        snake.extend()
         scoreboard.update_scoreboard(score)
+
+
+
 
 #     Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor()< -280:
         game_is_on = False
         scoreboard.game_over()
+#         Detect collision with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
